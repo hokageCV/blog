ca384844034c4a95a18e059d29f0dde837132742
hokageCV <chaitanyavaru@gmail.com>
2023-05-10 02:07:17 +0530

    refactor: change date display format in lists

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -5,7 +5,11 @@ export const dateToWordedMonthDate = (date: Date) => {
 };
 
 export const dateToNumberedMonthDate = (date: Date) => {
-  return date.toLocaleString("en-IN", {
-    dateStyle: "short",
-  });
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const paddedMonth = month.toString().padStart(2, "0");
+  const paddedDay = day.toString().padStart(2, "0");
+
+  return `${paddedDay}-${paddedMonth}-${year}`;
 };
