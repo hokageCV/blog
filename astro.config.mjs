5e127cdac168e95b94e37db09d211c3d8e5599f7
Chaitanya Varu <98262685+hokageCV@users.noreply.github.com>
2023-08-08 17:34:56 +0530

    Update astro.config.mjs
    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
 
-  site: 'https://hokageCV.github.io',
+  site: 'https://hokagecv.github.io',
 });
